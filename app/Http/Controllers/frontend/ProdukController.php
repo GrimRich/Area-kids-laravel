34a2b01ecb3ef79a3c643e98c86d3f333baeab3c
RamaD <rama.d@bts.id>
2021-10-20 10:48:26 +0700

    Tambah web.php

--- app/Http/Controllers/frontend/ProdukController.php
+++ app/Http/Controllers/frontend/ProdukController.php
@@ -3,35 +3,41 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
-use App\Model\master\produk;
+use App\model\master\Produk;
 use Illuminate\Support\Collection;
 
 class ProdukController extends Controller
 {
-    public function produkBaru(){
+    public function produkBaru()
+    {
         $produkBaru         = Produk::orderBy("created_at", "desc")->get();
         $semuaProduk        = Produk::limit("8")->get();
 
-        
-        return view('home', 
+
+        return view(
+            'home',
             [
                 'produkBaru' => $produkBaru,
                 'semuaProduk' => $semuaProduk
-            ]);
+            ]
+        );
     }
 
-    public function produkDetail($alias){
+    public function produkDetail($alias)
+    {
         $produkDetail       = Produk::with(['produkGambar', 'produkVarian.produkVarianPilihan'])->where("alias", $alias)->first();
 
         $idProduk           = $produkDetail->first()->id;
         $idKatProdukSelect  = $produkDetail->first()->id_kategori;
         $produkRelated      = Produk::where("id_kategori", $idKatProdukSelect)->where("id", "!=", $idProduk)->limit(8)->get();
 
-        return view('detailProduk', 
+        return view(
+            'detailProduk',
             [
                 'produkDetail' => $produkDetail,
                 'produkRelated' => $produkRelated,
-            ]);
+            ]
+        );
 
         return view('detailProduk');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Http\Controllers\frontend\ProdukController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ProdukController::class, 'produkBaru']);
 
 Route::get('/detail/{alias}', [ProdukController::class, 'produkDetail']);
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('config:cache');
+});
+
+Route::get('/storage-link', function () {
+    $exitCode = Artisan::call('storage:link');
+});
